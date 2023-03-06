a70e50c70d6ec3c5509e2828198275ffdce1c5f3
MotghareChetan <chetan@appmetry.com>
2023-03-06 17:45:54 +0530

    manual drag and drop code

--- tests/drag_drop.spec.js
+++ tests/drag_drop.spec.js
@@ -8,13 +8,18 @@ test.describe("Drag and Drop", () => {
         await page.goto("https://letcode.in/test");
     });
 
-    test("Drag and Drop", async ({ page }) => { 
-        
+    test("Drag and Drop", async ({ page }) => {
+
         await page.locator('text=AUI - 2').click();
-        await page.locator('#draggable').dragTo(page.locator('#droppable'));
-        console.log("Successfully dropped")
+        // await page.locator('#draggable').dragTo(page.locator('#droppable'));
+        // console.log("Successfully dropped")
         //await page.locator('#draggable').isVisible()
-        
+
+        await page.locator('#draggable').hover();
+        await page.mouse.down();
+        await page.locator('#droppable').hover();
+        await page.mouse.up();
+
 
 
     })
