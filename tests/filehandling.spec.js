82be17699465ed30978396e20bc3a2c6850c8cae
MotghareChetan <chetan@appmetry.com>
2023-03-08 14:13:04 +0530

    update file handling

--- tests/filehandling.spec.js
+++ tests/filehandling.spec.js
@@ -3,20 +3,26 @@ import { test, expect } from "@playwright/test";
 test.describe("File Upload", () => {
     test.beforeEach(async ({ page }) => {
         // Go to the starting url before each test.
-        await page.goto("https://letcode.in/file");
+        //  await page.goto("https://letcode.in/file");
+        await page.goto("https://tus.io/demo.html");
+
     });
 
-   
+
 
     test("File Upload", async ({ page }) => {
-        
+
         const filepath = 'Files/file1.docx'
 
-        const uploadFilebtn =  await page.locator("input[type='file']");
+        const uploadFilebtn = await page.locator("input[type='file']");
+
         await uploadFilebtn.setInputFiles(filepath)
+        //await page.locator('#pdf').click()
 
 
     })
+
+
 
 
 })
